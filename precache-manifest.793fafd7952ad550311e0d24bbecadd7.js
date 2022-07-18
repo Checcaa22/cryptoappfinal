ab1f7004b308f9cd09492cb6ce1fda529377f5f4
Franco <fcheccacci89@gmail.com>
2022-07-18 16:04:06 -0300

    Updates

---
+++ precache-manifest.793fafd7952ad550311e0d24bbecadd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c9c06cc40cd2a552001",
+    "url": "/cryptoappfinal/static/css/main.20cbd365.chunk.css"
+  },
+  {
+    "revision": "8c9c06cc40cd2a552001",
+    "url": "/cryptoappfinal/static/js/main.8c9c06cc.chunk.js"
+  },
+  {
+    "revision": "4422e644433f4a61be16",
+    "url": "/cryptoappfinal/static/js/1.4422e644.chunk.js"
+  },
+  {
+    "revision": "02bc121ab8ece00a49b4",
+    "url": "/cryptoappfinal/static/css/2.0ed11e14.chunk.css"
+  },
+  {
+    "revision": "02bc121ab8ece00a49b4",
+    "url": "/cryptoappfinal/static/js/2.02bc121a.chunk.js"
+  },
+  {
+    "revision": "72cc998700daa62c3684",
+    "url": "/cryptoappfinal/static/js/runtime~main.72cc9987.js"
+  },
+  {
+    "revision": "2311ab22c4c5eab76cc8cb16ca92ff3a",
+    "url": "/cryptoappfinal/index.html"
+  }
+];
